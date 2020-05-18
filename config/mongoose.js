3eea310f689804c820d2c200b1daa903c54da8b3
unknown <akmtanda97@gmail.com>
2020-05-18 15:23:19 +0530

    commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 
-mongoose.connect('mongodb://localhost/API_development');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/your-app-name');
 
 
 const db=mongoose.connection;
